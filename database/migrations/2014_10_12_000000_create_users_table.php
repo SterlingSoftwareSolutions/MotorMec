6e22bd26d957fd2a32a64069046693c4bf63033a
Naseef <aunaseef@users.noreply.github.com>
2023-02-10 15:59:55 +0530

    add user type

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('type')->default(0);
             $table->string('name');
             $table->string('businessname')->nullable();
             $table->string('username')->unique()->nullable();
