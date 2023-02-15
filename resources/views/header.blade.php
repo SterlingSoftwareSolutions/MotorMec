56fa2e3d4db8aa1a55bf306ad1d6bb1e78908550
Ayesh Himatha <104409437+AyeshHimantha@users.noreply.github.com>
2023-02-15 11:20:45 +0530

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,17 @@
       </button>
       <a class="navbar-brand" href="#">MotorMec</a>
     <div class="right">
-      <img src="{{asset('images/profile.png')}}" alt="">
+    <div class="dropdown">
+  <button class="btn dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+  <img src="{{asset('images/profile.png')}}" alt="">
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+    <li><button class="dropdown-item" type="button">Action</button></li>
+    <li><button class="dropdown-item" type="button">Another action</button></li>
+    <li><button class="dropdown-item" type="button">Something else here</button></li>
+  </ul>
+</div>
+      
       <i class="fa fa-bell"></i>
       <i class="fa fa-envelope"></i>
     </div>
