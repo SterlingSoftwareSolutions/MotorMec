587a2b9cec136fa411362080acdeba538c429b9b
Thafani24 <thafnaz42@gmail.com>
2023-03-02 16:35:37 +0530

    user image changed

--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -5,24 +5,6 @@
 
 @section('content')
 <p class="user-profile-h">User Profile Page</p>
-
-<!-- <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1"  placeholder="Enter Name">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Update</button>
-  <button type="submit" class="btn btn-primary">Cancel</button>
-
-</form> -->
 <form class="border border-secondary rounded-3 p-5">
   <div class="row">
     <h4 class="border-bottom border-secondary border-2">Details</h4>
@@ -60,36 +42,6 @@
   </div>
 </form>
 
-
-
-<!-- <div class="row">
-        <div class="col-md-4 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                <img class="rounded-circle mt-5"  ><span class="font-weight-bold">Upload a different photo</span><span> </span>
-            </div>
-        </div>
-        <div class="col-md-4 border-right">
-            <div class="p-3 py-5">
-                <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                    <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">PhoneNumber</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
-                    <div class="col-md-12"><label class="labels">Address</label><input type="text" class="form-control" placeholder="enter address" value=""></div>
-                </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="p-3 py-5">
-                <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""></div> <br>
-                <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""></div>
-            </div>
-        </div>
-    </div> -->
 </div>
 </div>
-
-
 @endsection
