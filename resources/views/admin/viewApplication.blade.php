b537a265403f7fb680d5516ebaa49b47ec86a60d
Ayesh Himatha <104409437+AyeshHimantha@users.noreply.github.com>
2023-03-07 14:55:49 +0530

    1

---
+++ resources/views/admin/viewApplication.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@include('sidebar')
+@include('header')
+@section('content')
+<div class="form-step-active">
+    <div class="row">
+        
+    </div>
+</div>
+@endsection
