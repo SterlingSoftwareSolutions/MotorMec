8bf45c1249d9ab8ffa34cd6d1fbfddbcaa08b8ef
Naseef <aunaseef@protonmail.com>
2023-03-09 12:45:23 +0530

    update applications table

--- database/migrations/2023_03_01_030001_create_applications_table.php
+++ database/migrations/2023_03_01_030001_create_applications_table.php
@@ -14,10 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('applications', function (Blueprint $table) {
+
+            // Meta fields
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->datetime('application_date')->nullable();
             $table->string('status')->nullable();
+
+            // User filled fields
             $table->string('approval_type')->nullable();
             $table->string('vass_engineering')->nullable();
             $table->datetime('arrival_date')->nullable();
@@ -31,7 +35,33 @@ return new class extends Migration
             $table->string('drive_type')->nullable();
             $table->string('seats')->nullable();
             $table->string('additional_info')->nullable();
+
+            // Admin filled fields
+
+            // -- filled when marking an application as submitted
+            $table->string('model_report_holder')->nullable();
+            $table->string('model_report_name')->nullable();
+            $table->string('sev')->nullable();
+            $table->string('submission_reference_number')->nullable();
+
+            // -- filled when admin uploads the VIA
+            $table->string('approval_number')->nullable();
+            $table->string('vin')->nullable();
+
+            // -- filled when admin uploads the compliance
+            $table->string('compliance_reference_number')->nullable();
+
+            // -- additional details, can be filled at any time
+            // -- by editing the application
+            $table->boolean('mr_udn_requested')->nullable();
+            $table->string('udn_number')->nullable();
+            $table->string('engine_number')->nullable();
+            $table->string('tare_mass')->nullable();
+            $table->string('power_kw')->nullable();
+
+            // Timestamps
             $table->timestamps();
+
         });
     }
 
