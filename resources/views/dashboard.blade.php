aeaa62ae7dfb96fde4829cb4ce4d7380e41b1b7c
Thafani24 <thafnaz42@gmail.com>
2023-03-09 18:09:43 +0530

    dashboard-circular progress bar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,4 +3,74 @@
 @include('header')
 @section('content')
 <h1>Dashboard Content</h1>
+<div class="dashboard">
+  <div class="container1">
+    <div class="card-name">
+      <div class="box-name ">
+        <div class="percent">
+          <svg>
+            <circle cx="70" cy="70" r="70"></circle>
+            <circle cx="70" cy="70" r="70"></circle>
+          </svg>
+          <div class="number">
+            <p class="c-text">Total Applications</p> <span class="c-number">100</span>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-name">
+      <div class="box-name">
+        <div class="percent">
+          <svg>
+            <circle cx="70" cy="70" r="70"></circle>
+            <circle cx="70" cy="70" r="70"></circle>
+          </svg>
+          <div class="number">
+            <p class="c-text">Pending Applications</p> <span class="c-number">75</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-name">
+      <div class="box-name">
+        <div class="percent">
+          <svg>
+            <circle cx="70" cy="70" r="70"></circle>
+            <circle cx="70" cy="70" r="70"></circle>
+          </svg>
+          <div class="number">
+            <p class="c-text">Completed Applications</p> <span class="c-number">85</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-name">
+      <div class="box-name">
+        <div class="percent">
+          <svg>
+            <circle cx="70" cy="70" r="70"></circle>
+            <circle cx="70" cy="70" r="70"></circle>
+          </svg>
+          <div class="number">
+            <p class="c-text">Rejected Applications</p> <span class="c-number">25</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-name">
+      <div class="box-name">
+        <div class="percent">
+          <svg>
+            <circle cx="70" cy="70" r="70"></circle>
+            <circle cx="70" cy="70" r="70"></circle>
+          </svg>
+          <div class="number">
+            <p class="c-text">Draft Applications</p> <span class="c-number">25</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> 
 @endsection
